fe5d638f480a98835b829b848032f7355dd6daea
Matibois <100830614+Matibois@users.noreply.github.com>
2023-12-12 11:02:57 +0100

    oio

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -7,28 +7,12 @@ using UnityEngine.InputSystem;
 public class InputManager : MonoBehaviour
 {
     public CustomInput input;
-    [Range(1f, 50f)]
-    public float moveSpeed;
-    public Rigidbody2D rb;
-    private Vector2 moveVector = Vector2.zero;
-
-    public Animator animator;
-
-    private int LastDirection = -1;
-
-    enum AnimStates
-    {
-        Down = 2,
-        Up = 0,
-        Left = 3,
-        Right = 1
-    }
+    
+    public Controller ctr;
 
     private void Awake()
     {
         input = new CustomInput();
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
     }
 
     private void OnEnable()
@@ -49,61 +33,13 @@ public class InputManager : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = moveVector * moveSpeed;
+        
     }
 
 
     public void PlayerMove(InputAction.CallbackContext value)
     {
-        Debug.Log("move");
-        moveVector = value.ReadValue<Vector2>();
-
-
-        // Animation Management
-        float x = moveVector.x;
-        float y = moveVector.y;
-
-        int actualDir;
-        float characterSpeed = moveVector.magnitude;
-
-        animator.SetFloat("Speed", characterSpeed);
-
-        Debug.Log(characterSpeed);
-
-        if (characterSpeed > 0.1)
-        {
-            if ( ((y > 0) ? 1 : -1) * y < ((x > 0) ? 1 : -1) * x)
-            {
-                if (x > 0)
-                {
-                    actualDir = (int)AnimStates.Right;
-                }
-                else
-                {
-                    actualDir = (int)AnimStates.Left;
-                }
-            }
-            else if (y > 0)
-            {
-                actualDir = (int)AnimStates.Up;
-            }
-            else
-            {
-                actualDir = (int)AnimStates.Down;
-            }
-
-            if (actualDir != LastDirection)
-            {
-                animator.SetTrigger("ChangeState");
-                animator.SetInteger("State", actualDir);
-            }
-        }
-
-    }
-
-
-    private void OnStop(InputAction.CallbackContext value)
-    {
-        rb.velocity = Vector2.zero;
+        Vector2 toSend = value.ReadValue<Vector2>();
+        ctr.Move(toSend);
     }
 }
