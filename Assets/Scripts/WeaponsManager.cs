7c11c5f16b8bba011c122140e0e82a0f436933aa
Barok <95617182+BigBurger2@users.noreply.github.com>
2023-12-14 09:52:13 +0100

    Shoot patched

--- Assets/Scripts/WeaponsManager.cs
+++ Assets/Scripts/WeaponsManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 // Structure qui stocke les copies des armes pour l'object pool et la référence à sa coroutine
 struct WeaponList
 {
+    public int index;
     public int nbWeapons;
     public List<Weapon> weapons;
     public IEnumerator Coroutine;
@@ -22,12 +23,14 @@ public class WeaponsManager : MonoBehaviour
     private List<WeaponData> weaponDatas;
 
     private Dictionary<GameObject, WeaponList> actualWeapons;
+    private Dictionary<GameObject, WeaponList> modifiedWeapons;
 
-    private int index = 0;
+    //private int index = 0;
 
     private void Awake()
     {
         actualWeapons = new Dictionary<GameObject, WeaponList>();
+        modifiedWeapons = new Dictionary<GameObject, WeaponList>();
         InitAllWeapons();
     }
 
@@ -45,6 +48,7 @@ public class WeaponsManager : MonoBehaviour
             WeaponList tmpWpList = new WeaponList();
             tmpWpList.Coroutine = FireCoroutine(parent, data);
             tmpWpList.weapons = new List<Weapon>();
+            tmpWpList.index = 0;
             
 
             // Calc nb weapon
@@ -90,11 +94,10 @@ public class WeaponsManager : MonoBehaviour
         index++;
         index %= nbChild;*/
 
+        modifiedWeapons = new Dictionary<GameObject, WeaponList>(actualWeapons);
         foreach (var weaponList in actualWeapons)
         {
-
             StartCoroutine(weaponList.Value.Coroutine);
-            
         }
 
     }
@@ -104,6 +107,7 @@ public class WeaponsManager : MonoBehaviour
         foreach (var weaponList in actualWeapons)
         {
             StopCoroutine(weaponList.Value.Coroutine);
+            actualWeapons = modifiedWeapons;
         }
     }
 
@@ -113,15 +117,24 @@ public class WeaponsManager : MonoBehaviour
         
         while (true)
         {
-            Transform temp = parent.transform.GetChild(index);
+            WeaponList wpList = modifiedWeapons[parent];
+            //Debug.Log(parent.name + " : " + wpList.index + " : " + wpList.nbWeapons);
+
+            Transform temp = parent.transform.GetChild(wpList.index);
             temp.position = Player.position;
 
-            actualWeapons[parent].weapons[index].Fire(playerController.vectorDir);
+            wpList.weapons[wpList.index].Fire(playerController.vectorDir);
 
 
-            index++;
-            index %= actualWeapons[parent].nbWeapons;
-            yield return new WaitForSeconds(1f / weaponData.fireRate);
+            wpList.index++;
+            wpList.index %= wpList.nbWeapons;
+
+            modifiedWeapons[parent] = wpList;
+
+            for (int i=0; i < 10; i++)
+            {
+                yield return new WaitForSeconds((1f / weaponData.fireRate)/10);
+            }
         }
     }
 }
