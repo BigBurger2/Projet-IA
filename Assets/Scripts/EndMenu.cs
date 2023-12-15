bc0e729a84346ad8ef7eba683ba21696ba78d41b
Aurelien Russier <97309733+Malkael@users.noreply.github.com>
2023-12-15 12:50:42 +0100

    EndScene + LifeShader

--- Assets/Scripts/EndMenu.cs
+++ Assets/Scripts/EndMenu.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void BackToMenu()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetSceneAt(0).ToString());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TryAgain()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetSceneAt(1).ToString());
     }
 }
